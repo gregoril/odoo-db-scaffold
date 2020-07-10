b14e0fcafdeb7341e866ad28e6f3da23c0d4fc5b
luigi <luigi@LGIT-LG>
2020-07-10 15:27:34 +0200

    Fixing to use parameters

--- db_scaffold.py
+++ db_scaffold.py
@@ -1,17 +1,41 @@
 import psycopg2
 import sys
+import os
 
 
-conn_string = "host=xxx.xxx.xxx.xxx dbname=xxxxxxx user=xxxxxxx password=xxxxxxx port=5432"
+CONN_STRING = "host=xxx.xxx.xxx.xxx dbname=xxxxxxx user=xxxxxxx password=xxxxxxx port=5432"
+MODEL_NAME = "mymodel"
+MODEL_LONG_NAME = "My Model"
+GROUP_USER = "mymodel.group_user"
+GROUP_MANAGER = "mymodel.group_manager"
+
+
 try:
    from dev_settings import *
 except ImportError:
-       pass
+    print("Missing dev_settings.py ")
+    sys.exit()
+    pass
 
-conn = psycopg2.connect(conn_string)
-tfilter = sys.argv[1]
-print(tfilter)
+if not os.path.exists("results/models"):
+    os.makedirs("results/models")
 
+if not os.path.exists("results/view"):
+    os.makedirs("results/view")
+    
+if not os.path.exists("results/security"):
+    os.makedirs("results/security")
+    
+    
+    
+conn = psycopg2.connect(CONN_STRING)
+if len(sys.argv) <2 :
+    print ('usage: py db_scaffold.py <table pattern> \n eg.  py db_scaffold.py customers%    <- scaffold all tables whose name start with customers')
+    sys.exit()
+else:
+    tfilter = sys.argv[1]
+    print(tfilter)
+tfilter = "valuta%"
 t= """select table_name from information_schema.tables where table_name like '""" + tfilter +"""' """
 cur_t = conn.cursor()
 cur_t.execute(t)
@@ -37,30 +61,29 @@ for tbl in tbls:
     cur.execute(q, ('BADGES_SFR',))  # (table_name,) passed as tuple
     fields = cur.fetchall()
 
-    module_text="""
-    # -*- coding: utf-8 -*-
-    from odoo import models, fields, api, _
-    from odoo.exceptions import ValidationError
+    module_text="""# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
-    import logging
-    _logger = logging.getLogger(__name__)
+import logging
+_logger = logging.getLogger(__name__)
 
 
-    class """ + tname.replace(".","_") + """(models.Model):
-        _name = '""" + tname+ """'
-        _description = '""" + tname.replace(".","_") + """'
-        _auto = False
-        _table = '""" + tname + """'  \n \n"""
-    
+class """ + tname.replace(".","_") + """(models.Model):
+    _name = '""" + tname+ """'
+    _description = '""" + tname.replace(".","_") + """'
+    _auto = False
+    _table = '""" + tname + """'  \n \n"""
+
     security_text= """id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink
-    access_""" + tname.replace(".","_")  +""",access_""" + tname.replace(".","_")  +""",sotras-sot.model_""" + tname.replace(".","_")  +""",sotras-main.sotras_group_user,1,0,0,0
-    manage_""" + tname.replace(".","_")  +""",manage_""" + tname.replace(".","_")  +""",sotras-sot.model_""" + tname.replace(".","_")  +""",sotras-main.sotras_group_manager,1,1,1,1
-
-    """
+access_""" + tname.replace(".","_")  +""",access_""" + tname.replace(".","_")  +""",""" + MODEL_NAME + """.model_""" + tname.replace(".","_")  +""",""" + GROUP_USER + """,1,0,0,0
+manage_""" + tname.replace(".","_")  +""",manage_""" + tname.replace(".","_")  +""",""" + MODEL_NAME + """.model_""" + tname.replace(".","_")  +""",""" + GROUP_MANAGER + """,1,1,1,1
+"""
     
-    form_tree = """<!-- """ + tname.replace(".","_") + """ tree -->
+    form_tree = """
+        <!-- """ + tname.replace(".","_") + """ tree -->
             <record id=\"""" + tname.replace(".","_") + """_tree" model="ir.ui.view">
-                <field name="name">SOTraS """ + tname.replace("."," ") + """ tree</field>
+                <field name="name">""" + MODEL_LONG_NAME + " " + tname.replace("."," ") + """ tree</field>
                 <field name="model">""" + tname + """</field>
                 <field name="arch" type="xml">
 
@@ -73,29 +96,29 @@ for tbl in tbls:
     """
 
     form_menu = """
-            <!-- """ + tname.replace(".","_") + """ action -->
-            <record id="view_""" + tname.replace(".","_") + """_action" model="ir.actions.act_window">
-                <field name="name">""" + tname.replace(".","_") + """</field>
-                <field name="res_model">""" + tname + """</field>
-                <field name="view_mode">tree</field>
-                <field name="view_id" ref=\"""" + tname.replace(".","_") + """_tree"/>
-            </record>
+        <!-- """ + tname.replace(".","_") + """ action -->
+        <record id="view_""" + tname.replace(".","_") + """_action" model="ir.actions.act_window">
+            <field name="name">""" + tname.replace(".","_") + """</field>
+            <field name="res_model">""" + tname + """</field>
+            <field name="view_mode">tree</field>
+            <field name="view_id" ref=\"""" + tname.replace(".","_") + """_tree"/>
+        </record>
 
 
-            <!-- """ + tname.replace(".","_") + """ menu -->
-            <menuitem id="view_""" + tname.replace(".","_") + """_menu\"
-                name=\"""" + tname.replace(".","_") + """\"
-                parent=\"sotras-sot.main_sot_menut\"
-                sequence=\"40\"
-                action=\"view_""" + tname.replace(".","_") + """_action\"
-                groups=\"sotras-main.sotras_group_manager\"
-            />
-    """
+        <!-- """ + tname.replace(".","_") + """ menu -->
+        <menuitem id="view_""" + tname.replace(".","_") + """_menu\"
+            name=\"""" + tname.replace(".","_") + """\"
+            parent=\"""" + MODEL_NAME +""".main_menu\"
+            sequence=\"40\"
+            action=\"view_""" + tname.replace(".","_") + """_action\"
+            groups=\"""" + GROUP_MANAGER + """\"
+        />
+"""
 
     for f in fields:
         print (f[0], f[1], f[3])
         label = (f[3] or f[0]).replace('\'','\'\'')
-        module_text = module_text + "\n\t" + f[0] + "="
+        module_text = module_text + "\n    " + f[0] + "="
         if (f[1] == "character varying"):
             module_text += "fields.Char("
         elif (f[1] == "text"):
@@ -127,21 +150,33 @@ for tbl in tbls:
     print (module_text)
 
     view_text = """<?xml version="1.0" encoding="utf-8"?>
-    <odoo>
-        <data>
-    """ + form_tree + """
-    """ + form_menu + """
+<odoo>
+    <data>
+""" + form_tree + """
+""" + form_menu + """
 
-        </data>
-    </odoo>"""
+    </data>
+</odoo>"""
+
 
     print (view_text)
-    f = open("results/" + tname.replace(".","_")+".py", "w+")
+    f = open("results/models/" + tname.replace(".","_")+".py", "w+")
     f.write(module_text)
     f.close()
-    f = open("results/" + tname.replace(".","_")+"_view.xml", "w+")
+    f= file_object = open('results/model/__init__.py', 'a')
+    f.write("from . import " + tname.replace(".","_") + "\n")
+    f.close    
+    
+    f = open("results/view/" + tname.replace(".","_")+"_view.xml", "w+")
     f.write(view_text)
     f.close()
-    f = open("results/" + tname.replace(".","_")+"_security.csv", "w+")
+    f=file_object = open('results/__manifest_view.txt', 'a')
+    f.write("'views/" + tname.replace(".","_") +"_view.xml',\n")
+    f.close    
+    
+    f = open("results/security/" + tname.replace(".","_")+"_security.csv", "w+")
     f.write(security_text)
     f.close()
+    f=file_object = open('results/__manifest_security.txt', 'a')
+    f.write("'security/" + tname.replace(".","_") +"_security.csv',\n")
+    f.close    
